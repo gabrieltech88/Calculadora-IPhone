fe5d64a5568a07c0e666a79e8632b3b0cfc05d60
Gabriel Ramos Windows <gabriblay209@gmail.com>
2024-12-10 17:00:43 -0300

    Troca da tag do teclado da calculadora de lista para tabela e calculadora funcionando, porém os boões de porcentagem e parenteses não foram tratados, além de ter situações que serão tratadas ao longo do tempo

--- client/public/src/scripts/script.js
+++ client/public/src/scripts/script.js
@@ -4,22 +4,46 @@ btn.forEach(btn => btn.addEventListener('click', pegarValor));
 let listaOperacao = []
 
 function pegarValor() {
-    valorBtn = this.value;
+    const valorBtn = this.value;
     listaOperacao.push(valorBtn);
     mostrarNaTela(listaOperacao);
     
 }
 
-function mostrarNaTela(valorBtn) {
-    const ultimoValor = valorBtn.length - 1
-    const output = document.getElementById('saida');
-    //console.log(output.value)
+function apagar(output) {
+    output.innerHTML = 'Resultado';
+    return listaOperacao = [];
+}
 
-    if(valorBtn[ultimoValor] == '='){
-        output.innerHTML = eval(output.innerHTML)
-    } else {
-        output.innerHTML = valorBtn.join('');
-        return toString(output.value)
+function mostrarNaTela(listaValores) {
+    console.log(listaValores.length)
+    try {
+        const ultimoValor = listaValores.length - 1
+        const output = document.getElementById('saida');
+        
+        if (listaValores[ultimoValor] === 'DEL'){
+            output.innerHTML = listaValores.pop('DEL');
+            output.innerHTML = listaValores.pop(ultimoValor);
+        }
+        
+        const penultimoValor = listaValores.length - 2;
+        if(listaValores[ultimoValor] == '=' && listaValores[penultimoValor] == '=') {
+            apagar(output);
+            throw new Error('Tentiva de opereção irreconhecivel')
+        }
+    
+        if(listaValores[ultimoValor] == '='){
+            output.innerHTML = eval(output.innerHTML)
+        } else if (listaValores[ultimoValor] === 'AC') {
+            apagar(output);
+        } else {
+            output.innerHTML = listaValores.join('');
+        }
+
+
+
+    } catch(erro) {
+        alert(erro, "Houve algum erro de digitiação")
     }
 }
 
