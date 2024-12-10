23c846335335f2a3ba6bbc27c8be25ba7c9b97f9
Gabriel Ramos Windows <gabriblay209@gmail.com>
2024-12-10 14:58:09 -0300

    Calculadora funcionando e HTML e CSS quase 100%

---
+++ client/public/src/scripts/script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelectorAll(".btn");    
+btn.forEach(btn => btn.addEventListener('click', pegarValor)); 
+
+let listaOperacao = []
+
+function pegarValor() {
+    valorBtn = this.value;
+    listaOperacao.push(valorBtn);
+    mostrarNaTela(listaOperacao);
+    
+}
+
+function mostrarNaTela(valorBtn) {
+    const ultimoValor = valorBtn.length - 1
+    const output = document.getElementById('saida');
+    console.log(output.value)
+
+    if(valorBtn[ultimoValor] == '='){
+        output.innerHTML = eval(output.innerHTML)
+    } else {
+        output.innerHTML = valorBtn.join('');
+        return toString(output.value)
+    }
+}
+
--- servidor/app.js
+++ servidor/app.js
@@ -5,9 +5,7 @@ import url from "url";
 const app = express();
 
 const caminhoAtual = url.fileURLToPath(import.meta.url);
-console.log(caminhoAtual)
 const diretorioPublico = path.join(caminhoAtual, "../..", "client", "public");
-console.log(diretorioPublico)
 app.use(express.static(diretorioPublico));
 
 
