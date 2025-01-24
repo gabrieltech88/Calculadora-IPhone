8562b00baa36eafe470abaa710255d2e3ee1ac6c
Gabriel Ramos Windows <gabriblay209@gmail.com>
2025-01-24 17:01:02 -0300

    Reconfigurações de conexão ao db

--- src/db/migrations/20250124011154-add-salt-to-usuarios.js
+++ src/db/migrations/20250124011154-add-salt-to-usuarios.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-      await queryInterface.addColumn('Usuarios', 'salt', {
+      await queryInterface.addColumn('usuarios', 'salt', {
         type: Sequelize.STRING,
         allowNull: false // Ou true, conforme a necessidade
       });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Usuarios', 'salt');
+    await queryInterface.removeColumn('usuarios', 'salt');
   }
 };
