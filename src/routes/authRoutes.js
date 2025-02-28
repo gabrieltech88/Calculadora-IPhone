53568763df4630e2db2cd442932c7afdff4d9348
gabrieltech88 <gabrieltech209@gmail.com>
2025-02-28 12:51:28 -0300

    feat(auth): adição de autorização na aplicação | Foi adicionado na nossa aplicação o uso de Token JWT e adição de novas dependências para processar dados e cookies | Fixes-issue #10

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const UsuarioController = require('../controllers/usuarioController.js');
+const autenticado = require('../middlewares/autenticado.js');
 
 const router = Router();
+
 
 router.post('/auth', UsuarioController.login);
 router.get('/auth/logout', UsuarioController.logout);
--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const PageController = require('../controllers/pageController.js');
+const autenticado = require('../middlewares/autenticado.js');
 
 const router = Router();
 
 router.get('/', PageController.getLogin);
-router.get('/calculadora', PageController.getIndex);
+router.get('/calculadora', autenticado, PageController.getIndex);
 router.get('/signup', PageController.getSignup);
 
 
